3ee56e68efaca911d28f987dd7cd0cf063d7c565
Anil Sharma <anilsharma320143@gmail.com>
2025-09-19 10:19:37 +0530

    database is now on server -> tidbcloud.com

--- config/database.js
+++ config/database.js
@@ -1,18 +1,35 @@
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql2',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'strapi_app'),
+//       user: env('DATABASE_USERNAME', 'root'),
+//       password: env('DATABASE_PASSWORD', 'gkp301@123'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//     pool: { min: 2, max: 10 },
+//     acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+//   },
+// });
+
 const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql2',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi_app'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'gkp301@123'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT', 4000),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true) ? { rejectUnauthorized: true } : false,
     },
     pool: { min: 2, max: 10 },
     acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
   },
 });
-
